e2b4831ec86c5f404102ea256d34ff46517acbea
Nathan Papapietro <npapapietro95@gmail.com>
2021-05-18 15:30:54 -0500

    Bug/iam policy (#4)
    * Adding policy to users
    
    * Adding registry link to readme
    
    * typo
    
    * Add password policy
    
    * #checkov:skip=CKV_AWS_9
    
    Co-authored-by: James Cooke <jamesedwardcooke@gmail.com>

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_account_password_policy" "this" {
+  #checkov:skip=CKV_AWS_9
+  minimum_password_length        = 14
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  password_reuse_prevention      = 24
+}
