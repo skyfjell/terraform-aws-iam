707d9c5cb7a96a65a0fe0833ca9f647e385d87f1
James Cooke <jamesedwardcooke@gmail.com>
2021-07-22 16:30:29 -0500

    Implement labels. Flagged, optional, and defaults for password policy.

--- locals.tf
+++ locals.tf
@@ -1,4 +1,26 @@
 locals {
   users  = defaults(var.users, { path = "/" })
   groups = defaults(var.groups, {})
+  labels = defaults(
+    var.labels,
+    { id = "" },
+  )
+
+  enable_password_policy = var.enable_password_policy
+  password_policy = defaults(
+    var.password_policy,
+    {
+      minimum_password_length        = 14
+      require_lowercase_characters   = true
+      require_numbers                = true
+      require_uppercase_characters   = true
+      require_symbols                = true
+      allow_users_to_change_password = true
+      password_reuse_prevention      = 24
+    },
+  )
+}
+
+locals {
+  prefix = length(local.labels.id) == 0 ? "" : "${local.labels.id}-"
 }
--- variables.tf
+++ variables.tf
@@ -19,4 +19,32 @@ variable "groups" {
   }
 }
 
+variable "labels" {
+  description = "Instance of labels module"
+  default     = {}
+  type = object(
+    {
+      id = optional(string)
+    }
+  )
+}
 
+variable "enable_password_policy" {
+  description = "Whether to enable the password policy resource"
+  default     = true
+  type        = bool
+}
+
+variable "password_policy" {
+  description = "Password policy overrides"
+  default     = {}
+  type = object({
+    minimum_password_length        = optional(number)
+    require_lowercase_characters   = optional(bool)
+    require_numbers                = optional(bool)
+    require_uppercase_characters   = optional(bool)
+    require_symbols                = optional(bool)
+    allow_users_to_change_password = optional(bool)
+    password_reuse_prevention      = optional(number)
+  })
+}
