516c650ce057e4403297d8adce4dbc37d03fefcf
James Cooke <jamesedwardcooke@gmail.com>
2022-09-21 10:47:43 -0500

    TF 1.3.0 Compat

--- variables.tf
+++ variables.tf
@@ -2,17 +2,21 @@ variable "users" {
   description = "List of user objects"
   type = list(object({
     name          = string
-    path          = optional(string)
+    path          = optional(string, "/")
     groups        = optional(list(string))
     force_destroy = optional(bool)
   }))
+  default = []
 }
 
 variable "groups" {
   description = "List of group objects. Protected group names: ['admins', 'users', 'billing', 'billing-ro']"
+
   type = list(object({
     name = string
   }))
+
+  default = []
 
   validation {
     condition     = alltrue([for group in var.groups : !contains(["users", "admins", "billing", "billing-ro"], group.name)])
@@ -24,38 +28,38 @@ variable "labels" {
   description = "Instance of labels module"
   type = object(
     {
-      id   = string
-      tags = any
+      id   = optional(string, "")
+      tags = optional(any, {})
     }
   )
-  default = {
-    id   = ""
-    tags = {}
-  }
+  default = {}
 }
 
 variable "enable_password_policy" {
   description = "Whether to enable the password policy resource"
-  default     = true
   type        = bool
+  default     = true
 }
 
 variable "password_policy" {
   description = "Password policy overrides"
-  default     = {}
+
   type = object({
-    minimum_password_length        = optional(number)
-    require_lowercase_characters   = optional(bool)
-    require_numbers                = optional(bool)
-    require_uppercase_characters   = optional(bool)
-    require_symbols                = optional(bool)
-    allow_users_to_change_password = optional(bool)
-    password_reuse_prevention      = optional(number)
+    minimum_password_length        = optional(number, 14)
+    require_lowercase_characters   = optional(bool, true)
+    require_numbers                = optional(bool, true)
+    require_uppercase_characters   = optional(bool, true)
+    require_symbols                = optional(bool, true)
+    allow_users_to_change_password = optional(bool, true)
+    password_reuse_prevention      = optional(number, 24)
   })
+
+  default = {}
 }
+
 
 variable "use_prefix" {
   description = "Use prefix on all names generate by labels module"
-  default     = true
   type        = bool
+  default     = true
 }
